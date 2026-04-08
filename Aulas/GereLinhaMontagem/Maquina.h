be6f67a50269a908a62fc85a9ea99e196dc5d383
lufer <lufer@ipca.pt>
2026-04-08 09:35:30 +0100

    Gere Linha de Montagem - Solução Slides 5

---
+++ Aulas/GereLinhaMontagem/Maquina.h
@@ -0,0 +1,58 @@
+/*****************************************************************//**
+ * \file   Maquina.h
+ * \brief  
+ * 
+ * \author lufer
+ * \date   April 2026
+ *********************************************************************/
+#include <stdbool.h>
+
+#ifndef A
+#define A 1
+
+typedef struct Maquina {
+	int num;
+}Maquina, *PtrMaquina;
+
+typedef struct ListaMaquinas {
+	Maquina maquina;
+	struct ListaMaquinas* prox; //ou apenas  PtrMaquina prox;
+}ListaMaquinas;
+
+#pragma region Assinatura_de_funções
+
+//Cria nova mquina
+Maquina* CriaMaquinaNova(int num);
+
+/// Cria registo para inserir na linha de montagem
+ListaMaquinas* CriaMaquinaLinha(Maquina* nova);
+
+//Apresenta todas as máquinas
+void MostraLinhaMontagem(ListaMaquinas* h);
+
+//Insere máquina no início
+ListaMaquinas* InsereNovaInicio(ListaMaquinas* head, Maquina* nova);
+
+//Insere máquina no final
+ListaMaquinas* InsereNovaFim(ListaMaquinas* head, Maquina* nova);
+
+//Insere máquina de forma ordenada
+ListaMaquinas* InserePorOrdem(ListaMaquinas* head, Maquina* nova);
+
+/// Verifica se determinado número de máquina já existe
+bool ExisteMaquina(int numMaquina, ListaMaquinas* head);
+
+//Destroi lista
+ListaMaquinas* DeleteAll(ListaMaquinas* h);
+
+//Gerir Preservação de Dados - Ficheiros
+//Guarda Dados
+bool PreservaInformação(char fileName[], ListaMaquinas* h);
+//Carrega Dados
+ListaMaquinas* CarregaDados(char fileName[], ListaMaquinas* h);
+
+
+
+#pragma endregion
+
+#endif
