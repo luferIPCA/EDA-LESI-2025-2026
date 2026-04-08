7e683f93a7c2b49fbd08d559546499669abba674
lufer <lufer@ipca.pt>
2026-04-08 09:37:10 +0100

    Queue com Listas Ligadas

---
+++ Aulas/Outras Estruturas/Aula 10 - Queue Lista/Queue.h
@@ -0,0 +1,42 @@
+//-----------------------------------------------------------------------
+// <copyright file="stack.h" company="IPCA">
+//     Copyright IPCA. All rights reserved.
+// </copyright>
+// <date></date>
+// <author>lufer</author>
+// <email>lufer@ipca.pt</email>
+// <version></version>
+// <desc>Manipulação de Queue com Listas</desc>
+//-----------------------------------------------------------------------
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <malloc.h>
+
+#ifndef QUEUE
+#define QUEUE
+
+//Elemento da Queue
+typedef struct Node
+{
+	int valor;
+	struct Node *next;
+}Queue, *QueuePtr;
+
+//preservar o inicio e fim da fila
+typedef struct Control {
+	struct Node *head;		//inicio da fila
+	struct Node *tail;		//fim da fila
+}Control;
+
+//Assinaturas
+int FrontElement(Control*h);
+Control *Dequeue(Control *h);
+Control *Enqueue(Control *ht, int data);
+bool Empty(Control *h);
+void ShowQueue(Control *h);
+Control *Create();
+int QueueSize(Control *h);
+
+#endif
